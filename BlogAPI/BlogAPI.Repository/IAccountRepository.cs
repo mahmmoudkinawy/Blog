47ece76ef9a65a0893f5d5115246520336e53b00
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-10 22:06:01 +0200

    Added Connection string & Account Repository

---
+++ BlogAPI/BlogAPI.Repository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogAPI.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+        Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername, CancellationToken cancellationToken);
+    }
+}
