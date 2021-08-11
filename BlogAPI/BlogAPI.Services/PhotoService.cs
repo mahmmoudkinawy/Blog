786cfad09209efb9b4ceb3c57de98685b155d310
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-11 22:46:36 +0200

    Implemented IPhoto Service

---
+++ BlogAPI/BlogAPI.Services/PhotoService.cs
@@ -0,0 +1,34 @@
+﻿using BlogAPI.Models.Settings;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoService(IOptions<CloudinaryOptions> config)
+        {
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret);
+
+            _cloudinary = new Cloudinary(account);
+        }
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
