ec7e02153a3b489e9bbf01ccb5f8a760797a37b4
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-11 23:23:11 +0200

    Removed unused usings from StartUp.ts

--- BlogAPI/BlogAPI.Web/Startup.cs
+++ BlogAPI/BlogAPI.Web/Startup.cs
@@ -5,10 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlogAPI.Web
 {
@@ -20,6 +16,7 @@ namespace BlogAPI.Web
         {
             _config = config;
         }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
