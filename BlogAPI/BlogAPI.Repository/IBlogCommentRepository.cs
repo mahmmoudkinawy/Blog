2284e910c8418320fe1d4368705abbefa3698d23
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-11 19:40:35 +0200

    Added methods to the interface

--- BlogAPI/BlogAPI.Repository/IBlogCommentRepository.cs
+++ BlogAPI/BlogAPI.Repository/IBlogCommentRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace BlogAPI.Repository
+﻿using BlogAPI.Models.BlogComment;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Repository
 {
     public interface IBlogCommentRepository
     {
-
+        Task<BlogComment> UpsertAsync(BlogCommentCreate blogCommentCreate, int applicationUserId);
+        Task<List<BlogComment>> GetAllAsync(int blogId);
+        Task<BlogComment> GetAsync(int blogCommentId);
+        Task<int> DeleteAsync(int blogCommentId);
     }
 }
