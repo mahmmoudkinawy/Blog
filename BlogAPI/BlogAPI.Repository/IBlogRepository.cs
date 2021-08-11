9c001b4b3c8233de46da97fd0e0acd6712af3cdd
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-11 19:36:08 +0200

    Created and Implemented Blog Repository & It's Interface

---
+++ BlogAPI/BlogAPI.Repository/IBlogRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogAPI.Models.Blog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Repository
+{
+    public interface IBlogRepository
+    {
+        Task<Blog> UpsertAsync(BlogCreate blogCreate, int applicationUserId);
+        Task<PagedResults<Blog>> GetAllAsync(BlogPaging blogPaging);
+        Task<Blog> GetAsync(int blogId);
+        Task<List<Blog>> GetAllByUserIdAsync(int applicationUserId);
+        Task<List<Blog>> GetAllFamousAsync();
+        Task<int> DeleteAsync(int blogId);
+    }
+}
