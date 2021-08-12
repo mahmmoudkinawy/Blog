39f3d834882424ed1a5fe4029bc2a5ec16fa934c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-12 22:20:38 +0200

    Added blog-comment-service implemantion

--- Blog-UI/src/app/services/blog-comment.service.ts
+++ Blog-UI/src/app/services/blog-comment.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogCommentCreate } from '../models/blog-comment/blog-comment-create.model';
+import { BlogComment } from '../models/blog-comment/blog-comment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogCommentService {
 
-  constructor() { }
+  private apiURL = environment.webApi;
+
+  constructor(private http: HttpClient) { }
+
+  create(model: BlogCommentCreate): Observable<BlogComment> {
+    return this.http.post<BlogComment>(`${this.apiURL}/BlogComment`, model);
+  }
+
+  delete(blogCommentId: number): Observable<number> {
+    return this.http.delete<number>(`${this.apiURL}/BlogComment/${blogCommentId}`);
+  }
+
+  getAll(blogId: number) : Observable<BlogComment[]> {
+    return this.http.get<BlogComment[]>(`${this.apiURL}/${blogId}`);
+  }
+
+
+
 }
