17345f23be5bfc6b025298ac95ace36fb3e19b6e
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-12 22:53:15 +0200

    Added Photo Service end posints

--- Blog-UI/src/app/services/photo.service.ts
+++ Blog-UI/src/app/services/photo.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Photo } from '../models/photo/photo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  private apiURL = environment.webApi + '/Photo';
+
+  constructor(private http: HttpClient) { }
+
+  create(model: FormData): Observable<Photo> {
+    return this.http.post<Photo>(`${this.apiURL}`, model);
+  }
+
+  getByApplicationUserId():  Observable<Photo[]> {
+    return this.http.get<Photo[]>(`${this.apiURL}`);
+  }
+
+  get(photoId: number): Observable<Photo> {
+    return this.http.get<Photo>(`${this.apiURL}/${photoId}`);
+  }
+
+  delete(photoId: number):  Observable<number> {
+    return this.http.delete<number>(`${this.apiURL}/${photoId}`);
+  }
+
+
 }
