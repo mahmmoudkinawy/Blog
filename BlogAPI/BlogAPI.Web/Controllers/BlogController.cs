0cecdf1a84977e3bd46f984e60bb73f596689468
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-12 14:26:24 +0200

    Added Blog Controller

---
+++ BlogAPI/BlogAPI.Web/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using BlogAPI.Models.Blog;
+using BlogAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Web.Controllers
+{
+    public class BlogController : BaseApiController
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IPhotoRepository _photoRepository;
+
+        public BlogController(IBlogRepository blogRepository, IPhotoRepository photoRepository)
+        {
+            _blogRepository = blogRepository ??
+                throw new ArgumentNullException(nameof(blogRepository));
+            _photoRepository = photoRepository ??
+                throw new ArgumentNullException(nameof(photoRepository));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Blog>> Create(BlogCreate blogCreate)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            if (blogCreate.PhotoId.HasValue)
+            {
+                var photo = await _photoRepository.GetAsync(blogCreate.PhotoId.Value);
+
+                if (photo.ApplicationUserId != applicationUserId)
+                    return BadRequest("You did not upload the photo.");
+            }
+
+            var blog = await _blogRepository.UpsertAsync(blogCreate, applicationUserId);
+
+            return Ok(blog);
+        }
+
+
+
+
+    }
+}
