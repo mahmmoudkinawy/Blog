d8ddc1301b79eee1c644afa1ae8fb956626f5e58
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-12 13:39:59 +0200

    Added Insert Image to Cloudinary end point

--- BlogAPI/BlogAPI.Web/Controllers/PhotoController.cs
+++ BlogAPI/BlogAPI.Web/Controllers/PhotoController.cs
@@ -1,7 +1,58 @@
-﻿namespace BlogAPI.Web.Controllers
-{
+﻿using BlogAPI.Models.Photo;
+using BlogAPI.Repository;
+using BlogAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace BlogAPI.Web.Controllers
+{
     public class PhotoController : BaseApiController
     {
+        private readonly IPhotoRepository _photoRepository;
+        private readonly IBlogRepository _blogRepository;
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(
+            IPhotoRepository photoRepository,
+            IBlogRepository blogRepository,
+            IPhotoService photoService)
+        {
+            _photoRepository = photoRepository ??
+                throw new ArgumentNullException(nameof(photoRepository));
+            _blogRepository = blogRepository ??
+                throw new ArgumentNullException(nameof(blogRepository));
+            _photoService = photoService ??
+                throw new ArgumentNullException(nameof(photoService));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Photo>> UploadPhoto(IFormFile file)
+        {
+            //This is mean, who is this fucking user ?
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var uploadResult = await _photoService.AddPhotoAsync(file);
+
+            if (uploadResult.Error != null)
+                return BadRequest(uploadResult.Error.Message);
+
+            var photoCreate = new PhotoCreate()
+            {
+                PublicId = uploadResult.PublicId,
+                ImageUrl = uploadResult.SecureUrl.AbsoluteUri,
+                Description = file.FileName
+            };
+
+            var photo = await _photoRepository.InsertAsync(photoCreate, applicationUserId);
+
+            return Ok(photo);
+        }
+
     }
 }
