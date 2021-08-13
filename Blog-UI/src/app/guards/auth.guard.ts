dce3b4c4a40f5d870a1b371b582ce769e675ca92
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-13 14:14:47 +0200

    Added gurads and implemention of it

---
+++ Blog-UI/src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+      const currentUser = this.accountService.currentUserValue;
+      const isLoggedIn = currentUser && currentUser.token;
+
+      if(currentUser) {
+        return true;
+      }
+
+      this.router.navigate(['/']);
+      return false;
+  }
+  
+}
