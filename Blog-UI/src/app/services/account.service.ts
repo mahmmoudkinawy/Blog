1ce99a6505d30139d0d4fd8d077fe35615da2b78
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-15 18:20:14 +0200

    Added comment all login and UI

--- Blog-UI/src/app/services/account.service.ts
+++ Blog-UI/src/app/services/account.service.ts
@@ -1,63 +1,63 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-import { environment } from 'src/environments/environment';
-import { ApplicationUserCreate } from '../models/account/application-user-create.model';
-import { ApplicationUserLogin } from '../models/account/application-user-login.model';
-import { ApplicationUser } from '../models/account/application-user.model';
+import { environment } from "src/environments/environment";
+import { ApplicationUserCreate } from "../models/account/application-user-create.model";
+import { ApplicationUserLogin } from "../models/account/application-user-login.model";
+import { ApplicationUser } from "../models/account/application-user.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AccountService {
-
-  private apiURL = environment.webApi + '/Account';
+  private apiURL = environment.webApi + "/Account";
 
   private currentUserSubject$: BehaviorSubject<ApplicationUser>;
 
-  constructor(
-    private http: HttpClient
-  ) { 
-    this.currentUserSubject$ = new BehaviorSubject<ApplicationUser>(JSON.parse(localStorage.getItem('blogLab-currentUser')));
+  constructor(private http: HttpClient) {
+    this.currentUserSubject$ = new BehaviorSubject<ApplicationUser>(
+      JSON.parse(localStorage.getItem("blogLab-currentUser"))
+    );
   }
 
   login(model: ApplicationUserLogin): Observable<ApplicationUser> {
     return this.http.post(`${this.apiURL}/login`, model).pipe(
       map((user: ApplicationUser) => {
-
-        if(user){
-          localStorage.setItem('blogLab-currentUser', JSON.stringify(user));
+        if (user) {
+          localStorage.setItem("blogLab-currentUser", JSON.stringify(user));
           this.setCurrentUser(user);
         }
 
         return user;
       })
-    )
+    );
   }
 
-
-  register(model: ApplicationUserCreate) : Observable<ApplicationUser> {
+  register(model: ApplicationUserCreate): Observable<ApplicationUser> {
     return this.http.post(`${this.apiURL}/register`, model).pipe(
       map((user: ApplicationUser) => {
-
-        if(user) {
-          localStorage.setItem('blogLab-currentUser', JSON.stringify(user));
+        if (user) {
+          localStorage.setItem("blogLab-currentUser", JSON.stringify(user));
           this.setCurrentUser(user);
         }
-        
+
         return user;
       })
-    )
+    );
   }
 
   setCurrentUser(user: ApplicationUser) {
     this.currentUserSubject$.next(user);
   }
 
-  public get currentUserValue() : ApplicationUser {
+  public get currentUserValue(): ApplicationUser {
     return this.currentUserSubject$.value;
+  }
+
+  public givenUserIsLoggedIn(username: string) {
+    return this.isLoggedIn() && this.currentUserValue.username === username;
   }
 
   public isLoggedIn() {
@@ -67,9 +67,7 @@ export class AccountService {
   }
 
   logout() {
-    localStorage.removeItem('blogLab-currentUser');
+    localStorage.removeItem("blogLab-currentUser");
     this.currentUserSubject$.next(null);
   }
-
-
 }
